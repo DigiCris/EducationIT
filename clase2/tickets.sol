c3c3449ba378b0aae9269b3a1b2ee390213db15f
DigiCris <100968023+DigiCris@users.noreply.github.com>
2023-11-23 22:58:46 -0300

    Update tickets.sol
    Agrego ejercicio de tickets.sol a la clase 2. El mismoo está incompleto y hace falta completar.

--- clase2/tickets.sol
+++ clase2/tickets.sol
@@ -1,1 +1,40 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.1;
+
+//compra, venta, transferencia
+
+contract ticket {
+
+    address private owner;
+
+    mapping (address => uint8) entradas;
+
+    constructor() payable {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner==msg.sender,"no eres owner, hacker!");
+        _;
+    }
+
+    function retirar() external onlyOwner {
+        payable(owner).transfer(address(this).balance);
+    }
+
+    function compra() external payable {
+        // que acá comprueben el precio de la entrada
+        entradas[msg.sender] += 1; 
+    }
+
+    function transferir(address _to) external {
+        entradas[msg.sender] -= 1; 
+        entradas[_to] += 1; 
+    }
+
+    // vender entrada
+
+// pruebe en programa.
+
+}
